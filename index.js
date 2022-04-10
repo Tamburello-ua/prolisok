5d07c78a901278d2fc83af6ee117ca03891650ff
Yavdoshenko Dmytro <d.yavdoshenko@mobidev.biz>
2022-04-10 13:46:17 +0300

    p2.5

--- index.js
+++ index.js
@@ -26,57 +26,35 @@ function initMap() {
     addMarker(haightAshbury);
 };
 
+// Adds a marker to the map and push to the array.
+function addMarker(position) {
+    const marker = new google.maps.Marker({
+        position,
+        map,
+    });
 
-// (() => {
-//     "use strict";
-//     var e = {
-//         d: (o, t) => {
-//           for (var n in t)
-//             e.o(t, n) &&
-//               !e.o(o, n) &&
-//               Object.defineProperty(o, n, { enumerable: !0, get: t[n] });
-//         },
-//         o: (e, o) => Object.prototype.hasOwnProperty.call(e, o),
-//         r: (e) => {
-//           "undefined" != typeof Symbol &&
-//             Symbol.toStringTag &&
-//             Object.defineProperty(e, Symbol.toStringTag, {
-//               value: "Module",
-//             }),
-//             Object.defineProperty(e, "__esModule", { value: !0 });
-//         },
-//       },
-//       o = {};
-//     function t() {
-//       const e = { lat: 49.83154325978491, lng: 36.055631135182466 },
-//         o = new google.maps.Map(document.getElementById("map"), {
-//           zoom: 12,
-//           center: e,
-//           styles: [{
-//             featureType: 'poi',
-//             stylers: [{ visibility: 'off' }]  // Turn off POI.
-//           },
-//           {
-//             featureType: 'transit.station',
-//             stylers: [{ visibility: 'off' }]  // Turn off bus, train stations etc.
-//           }],
+    markers.push(marker);
+}
 
-//           streetViewControl: false,
-//         });
-//       let t = new google.maps.InfoWindow({
-//         content: "Click the map to get Lat/Lng!",
-//         position: e,
-//       });
-//       t.open(o),
-//         o.addListener("click", (e) => {
-//           t.close(),
-//             (t = new google.maps.InfoWindow({ position: e.latLng })),
-//             t.setContent(JSON.stringify(e.latLng.toJSON(), null, 2)),
-//             t.open(o);
-//         });
-//     }
-//     e.r(o), e.d(o, { initMap: () => t });
-//     var n = window;
-//     for (var l in o) n[l] = o[l];
-//     o.__esModule && Object.defineProperty(n, "__esModule", { value: !0 });
-//   })();
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (let i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function hideMarkers() {
+    setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+    setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    hideMarkers();
+    markers = [];
+}
