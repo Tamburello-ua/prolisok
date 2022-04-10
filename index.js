a46b07947efb5541a4b461761fe7a4cfcdc2db3a
Yavdoshenko Dmytro <d.yavdoshenko@mobidev.biz>
2022-04-10 19:27:30 +0300

    p3.0.24

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function lineCenter(position1, position2, delay) {
     var val1 = (soundSpeed * delay) / distance;
     var calcHead = Math.acos(val1);
 
-    var calcPosit = google.maps.geometry.spherical.computeOffset(centerPosition, 4000, calcHead);
+    var calcPosit = google.maps.geometry.spherical.computeOffset(centerPosition, 4000, heading - 90 - calcHead);
 
     drawLineBetweenTwoLocations(centerPosition, calcPosit, '#00FF00');
 }
